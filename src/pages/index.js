14d4e599ff37c5cd4b2fa00ad3f96e611f8d6405
Abhishek Shukla <abhishekshuklaJS@gmail.com>
2022-04-09 01:13:09 +0530

    Add home page banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
-import React from "react"
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+import Layout from "./Layout";
 
 export default function Home() {
-  return <div>Home Page</div>
+  return (
+    <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage src="../assets/images/main.jpeg" alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
+    </Layout>
+  )
 }
